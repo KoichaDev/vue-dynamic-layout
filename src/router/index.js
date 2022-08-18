e0bb3d1c80e17878ae34b02497e1924780486f17
Khoi Hoang <khoi.hoang@bouvet.no>
2022-08-18 16:16:22 +0200

    add: ✨ Home router

--- src/router/index.js
+++ src/router/index.js
@@ -1,2 +1,22 @@
-// import { createRouter, createWebHistory } from 'vue-router'
+import Vue from 'vue';
+import VueRouter from 'vue-router'
 
+// View components
+import Home from '../views/Home.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    routes,
+})
+
+export default router;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,10 @@
 <template>
-	<div id="app">
-		<router-view></router-view>
-	</div>
+	<div>Home</div>
 </template>
 
 <script>
 export default {
-	name: 'App',
+	name: 'HomeView',
 };
 </script>
 
