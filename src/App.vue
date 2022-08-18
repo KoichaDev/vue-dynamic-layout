62f5ba71e6132af68b1c8f8ca49e9251aaf9db60
Khoi Hoang <khoi.hoang@bouvet.no>
2022-08-18 15:57:57 +0200

    add: 📦 Vue Fragment as dependency

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-	<div></div>
+	<fragment>aaisdjioasdjioasj</fragment>
 </template>
 
 <script>
---
+++ src/plugins/vue-fragment.js
@@ -0,0 +1,4 @@
+import Vue from 'vue';
+import { Plugin as VueFragment } from 'vue-fragment'
+
+Vue.use(VueFragment)
